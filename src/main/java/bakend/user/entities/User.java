37f0f07a58e1f85533f05455e1e8e0efb8136db3
JJAS-DEV <ayosuajon4@gmail.com>
2026-07-01 20:54:59 -0400

    termino de la lession de hoy 1

--- src/main/java/bakend/user/entities/User.java
+++ src/main/java/bakend/user/entities/User.java
@@ -1,5 +1,6 @@
 package bakend.user.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,52 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    
+    private String name;
+    @Column(name = "last_name")
+    private String lastname;
+    private String email;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
 
 
 }
