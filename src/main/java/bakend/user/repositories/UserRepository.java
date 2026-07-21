1e2d2a9430a9b930f1c08c88ecce3315c97836d1
JJAS-DEV <ayosuajon4@gmail.com>
2026-07-20 20:25:51 -0400

    paginador sencillo

--- src/main/java/bakend/user/repositories/UserRepository.java
+++ src/main/java/bakend/user/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package bakend.user.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import bakend.user.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Page<User> findAll(Pageable pageable);
     
 
     
