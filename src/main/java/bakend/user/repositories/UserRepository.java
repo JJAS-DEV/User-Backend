4dbb6f0f80e1760c3ddab0070e0d89a724c127b1
JJAS-DEV <ayosuajon4@gmail.com>
2026-07-22 19:55:31 -0400

    avance del login con clase datails

--- src/main/java/bakend/user/repositories/UserRepository.java
+++ src/main/java/bakend/user/repositories/UserRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import bakend.user.entities.User;
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findAll(Pageable pageable);
+    Optional <User>  findByUsername(String username);
     
 
     
